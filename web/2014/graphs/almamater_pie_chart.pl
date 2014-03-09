251935d393d5e960da25cc58f8c1cc774370f680
Ben Klaas <ben@benklaas.com>
2014-03-08 18:07:02 -0600

    add 2014

---
+++ web/2014/graphs/almamater_pie_chart.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl
+
+use GD;
+use GD::Graph::pie;
+use strict;
+
+my $year = '2009';
+my %tallies;
+my @category_data;
+my @tally_data;
+my @graph_data;
+my $graph;
+my $logo = "jq_graph_logo.gif";
+
+get_tallies_from_db();
+
+
+for (sort {$tallies{$a} <=> $tallies{$b} } keys %tallies) {
+     push @category_data, $_;
+     push @tally_data, $tallies{$_};
+}
+
+@graph_data = ( [ @category_data ] , [ @tally_data ] );
+
+#$graph = GD::Graph::pie->new();
+$graph = GD::Graph::pie->new(650,400);
+
+$graph->set_title_font('/usr/share/fonts/truetype/ttf-bitstream-vera/VeraBd.ttf', 14);
+$graph->set_value_font('/usr/share/fonts/truetype/ttf-bitstream-vera/VeraBd.ttf', 12);
+
+$graph->set(
+	title	=>	"$year Alma Mater Breakdown",
+        logo	=>	$logo,
+	logo_position	=>	'UR',
+	suppress_angle	=>	3	
+        );
+
+
+my $gd = $graph->plot(\@graph_data); 
+open(IMG, '>almamater.png');
+binmode IMG;
+print IMG $gd->png;
+	
+
+sub get_tallies_from_db {
+
+use DBI;
+###############################################################
+############### connect to database ###########################
+###############################################################
+my $database_name = "johnnyquest";
+my $location = "localhost";
+my $port_num = "3306";
+
+# this would need to be slightly altered for other databases
+my $database = "DBI:mysql:$database_name:$location:$port_num";
+my $db_user = "nobody";
+
+my $dbh = DBI->connect($database,$db_user) or die $DBI::errstr;
+###############################################################
+
+  my $sql = "select alma_mater from player_info where man_or_chimp = \"man\"";
+
+  my $sth = $dbh->prepare ($sql);
+  $sth->execute ();
+
+  while (my $hashref = $sth->fetchrow_hashref()) {
+        my $almamater = $hashref->{alma_mater};
+        $tallies{$almamater}++;
+   }
+
+  $dbh->disconnect();
+
+
+}
+
---
+++ web/2014/index.cgi
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+$| = 1;  # make sure output is unbuffered
+
+use strict;
+use Template;
+print "Content-type: text/html\n\n";
+use CGI qw/ :param /;
+use DBI;
+use vars qw/ $dbh /;
+do "/data/cgi-bin/jq_globals.pl";
+my $PARAMS; 
+my $CONFIG = config_variables();
+
+for (param()) {
+	$PARAMS->{$_} = param("$_");
+}
+
+if (    $PARAMS->{'keywords'} eq 'enter' ||
+        $PARAMS->{'keywords'} eq 'enter2' ||
+        $PARAMS->{'keywords'} eq 'brackettest'
+	) {
+        connect_to_db('johnnyquest');
+        my @brackets = get_bracket_order();
+        my $teamRef = get_teams(@brackets);
+        my @unsorted = map { $_->{'team'} } @$teamRef ;
+        my @teams = sort @unsorted;
+        $PARAMS->{'teams'} = \@teams;
+}
+
+# change this to overview at launch
+$PARAMS->{'keywords'} = 'overview' unless $PARAMS->{'keywords'};
+
+my $tmpl = "index";
+
+unless (-e "$CONFIG->{'template_dir'}/$PARAMS->{'keywords'}") {
+	$PARAMS->{'keywords'} = 'filenotfound';
+}
+
+my $random = rand(1000);
+my %data = (
+	'params'	=>	$PARAMS,
+	'title'		=> 	$CONFIG->{'year'},
+	'year'		=>  $CONFIG->{'year'},
+	'random'	=>	$random,
+);
+
+my $template = Template->new({
+	INCLUDE_PATH	=> "$CONFIG->{'template_dir'}"
+}) or die "$!";
+
+$template->process($tmpl, \%data)
+        || print "Template process failed: ", $template->error(), "\n";
+
