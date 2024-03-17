0b3b9b267d528e4c10ac3ef502dbaf087895e7c8
Ben Klaas <ben@benklaas.com>
2024-03-17 14:37:30 -0500

    updates

--- api/app.py
+++ api/app.py
@@ -1,6 +1,13 @@
 import lib.db as db
 from lib.models import (
-    Filter, Games, LastUpdated, Picks, PlayerInfo, Scores, SimilarityIndex, Teams, 
+    Filter,
+    Games,
+    LastUpdated,
+    Picks,
+    PlayerInfo,
+    Scores,
+    SimilarityIndex,
+    Teams,
 )
 from flask import Flask, request
 from sqlalchemy import create_engine, text, desc
@@ -8,12 +15,13 @@ from app_service import AppService
 import json
 
 app = Flask(__name__)
-appService = AppService();
+appService = AppService()
 
 # XXX Change to johnnyquest
 DB = "jq_2023"
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return """
 *********************
@@ -21,13 +29,39 @@ JQ-API UP!!!
 *********************
 """
 
-@app.route('/api/test')
+
+@app.route("/api")
 def test():
     with db.get_engine(DB) as session:
         ret = []
         step = session.query(Scores).order_by(desc(Scores.step)).first()
-        result = session.query(Scores).filter(Scores.step == step.step, Scores.man_or_chimp == 'man').order_by(desc(Scores.score))
+        result = (
+            session.query(Scores)
+            .filter(Scores.step == step.step, Scores.man_or_chimp == "man")
+            .order_by(desc(Scores.score))
+        )
         for r in result:
             ret.append({"name": r.name, "score": r.score})
     return json.dumps(ret)
 
+
+@app.route("/leaderboard")
+def leaderboard():
+    with db.get_engine(DB) as session:
+        ret = []
+        step = session.query(Scores).order_by(desc(Scores.step)).first()
+        result = (
+            session.query(Scores)
+            .filter(Scores.step == step.step, Scores.man_or_chimp == "man")
+            .order_by(desc(Scores.score))
+        )
+        for r in result:
+            ret.append(
+                {
+                    "name": r.name,
+                    "score": r.score,
+                    "candybar": r.player_info_obj.candybar,
+                    "champion": r.player_info_obj.champion,
+                }
+            )
+    return json.dumps(ret)
